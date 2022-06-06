4fd2b1832a6034f7aa9b46a51bd230753c2b09af
ion05 <aayanagarwal05@gmail.com>
2022-06-06 17:54:32 +0530

    eror fix

--- index.js
+++ index.js
@@ -1,52 +1,50 @@
-const express = require('express');
-const path = require('path');
-const ejs = require('ejs');
-const env = require('dotenv').config();
-const cookieParser = require('cookie-parser');
-const momngoose = require('mongoose');
-const { SendMessage } = require('./services/errorReporting');
+const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
+const env = require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const momngoose = require("mongoose");
+const { SendMessage } = require("./services/errorReporting");
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-const indexRouter = require('./routes/indexRoute');
-const authRouter = require('./routes/authRoute');
-const dashboardRouter = require('./routes/dashboardRoute')
-const adminRouter = require('./routes/adminRoute')
+const indexRouter = require("./routes/indexRoute");
+const authRouter = require("./routes/authRoute");
+const dashboardRouter = require("./routes/dashboardRoute");
+const adminRouter = require("./routes/adminRoute");
 
 app.use(indexRouter);
 app.use(authRouter);
 app.use(dashboardRouter);
-app.use(adminRouter)
-
-
+app.use(adminRouter);
 
 app.use((err, req, res, next) => {
-    console.log('hi')
-    SendMessage(err.stack.toString()) 
-    SendMessage('The server has crashed')
-    next(err)
-})  
-
-
-const link = `mongodb+srv://techsyndicate:${process.env.MONGO_PASS}@cluster0.8yhrd.mongodb.net/?retryWrites=true&w=majority`
-const PORT = process.env.PORT || 3000;
-momngoose.connect(link, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    
-    console.log('connected to mongoDB');
-    SendMessage('MongoDB connected')
-    app.listen(PORT, () => {
-        SendMessage('Server started on port  ' + PORT)
-        console.log('Server started at port ' + PORT);
-    })
-}).catch(err => {
-    SendMessage(err);
-    SendMessage('Error connecting to mongoDB');
+  console.log("hi");
+  SendMessage(err.stack.toString());
+  SendMessage("The server has crashed");
+  next(err);
 });
+
+const link = `mongodb+srv://techsyndicate:${process.env.MONGO_PASS}@cluster0.8yhrd.mongodb.net/?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 3000;
+momngoose
+  .connect(link, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("connected to mongoDB");
+    SendMessage("MongoDB connected");
+    app.listen(PORT, () => {
+      SendMessage("Server started on port  " + PORT);
+      console.log("Server started at port " + PORT);
+    });
+  })
+  .catch((err) => {
+    SendMessage(err);
+    SendMessage("Error connecting to mongoDB");
+  });
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,145 +1,148 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {Client} = require('@notionhq/client');
-const notion = new Client({auth: process.env.NOTION_TOKEN});
-const jwt = require('jsonwebtoken');
-const { SendMessage } = require('../services/errorReporting');
-const jwt_token = process.env.JWT_TOKEN
+const { Client } = require("@notionhq/client");
+const notion = new Client({ auth: process.env.NOTION_TOKEN });
+const jwt = require("jsonwebtoken");
+const { SendMessage } = require("../services/errorReporting");
+const jwt_token = process.env.JWT_TOKEN;
 
 router.get("/", async (req, res) => {
-    if (req.cookies.token) {
-        const token = req.cookies.token
-    
-        const decoded = jwt.verify(token, jwt_token);
-            const user = await notion.databases.query({
-                database_id: process.env.NOTION_DB_ID,
-                filter: {
-                    and: [
-                        {
-                            property: 'email',
-                            title: {
-                                equals: decoded.email
-                            }
-                        }
-                    ]
-                }
-            })
-        if (user) {
-            req.user =  {
-                email: user.results[0].properties.email.title[0].plain_text, 
-                name: user.results[0].properties.Name.rich_text[0].text.content,
-                displayName: user.results[0].properties.displayName.rich_text[0].text.content
-            }
-        }
-    }
-    return res.render("index",{userLog: req.user});
-})
+  if (req.cookies.token) {
+    const token = req.cookies.token;
 
-router.get('/leaderboard', async (req,res)=> { 
-try {   
+    const decoded = jwt.verify(token, jwt_token);
+    const user = await notion.databases.query({
+      database_id: process.env.NOTION_DB_ID,
+      filter: {
+        and: [
+          {
+            property: "email",
+            title: {
+              equals: decoded.email,
+            },
+          },
+        ],
+      },
+    });
+    if (user) {
+      req.user = {
+        email: user.results[0].properties.email.title[0].plain_text,
+        name: user.results[0].properties.Name.rich_text[0].text.content,
+        displayName:
+          user.results[0].properties.displayName.rich_text[0].text.content,
+      };
+    }
+  }
+  return res.render("index", { userLog: req.user });
+});
+
+router.get("/leaderboard", async (req, res) => {
+  try {
     if (req.cookies.token) {
-        const token = req.cookies.token
-        const decoded = jwt.verify(token, jwt_token);
-            const user = await notion.databases.query({
-                database_id: process.env.NOTION_DB_ID,
-                filter: {
-                    and: [
-                        {
-                            property: 'email',
-                            title: {
-                                equals: decoded.email
-                            }
-                        }
-                    ]
-                }
-            })
-        if (user) {
-            req.user =  {
-                email: user.results[0].properties.email.title[0].plain_text, 
-                name: user.results[0].properties.Name.rich_text[0].text.content,
-                displayName: user.results[0].properties.displayName.rich_text[0].text.content
-            }
-        }
+      const token = req.cookies.token;
+      const decoded = jwt.verify(token, jwt_token);
+      const user = await notion.databases.query({
+        database_id: process.env.NOTION_DB_ID,
+        filter: {
+          and: [
+            {
+              property: "email",
+              title: {
+                equals: decoded.email,
+              },
+            },
+          ],
+        },
+      });
+      if (user) {
+        req.user = {
+          email: user.results[0].properties.email.title[0].plain_text,
+          name: user.results[0].properties.Name.rich_text[0].text.content,
+          displayName:
+            user.results[0].properties.displayName.rich_text[0].text.content,
+        };
+      }
     }
     const userLog = req.user;
-    const dbId = process.env.NOTION_DB_ID
+    const dbId = process.env.NOTION_DB_ID;
     const usersN = await notion.databases.query({
-        database_id: dbId,
-        filter: {
-            and: [
-                {
-                    property: 'isAdmin',
-                    checkbox: {
-                        equals: false
-                    }
-                },
-                {
-                    property: "isBanned", 
-                    checkbox: {
-                        equals: false
-                    }
-                }, 
-                { 
-                    property: "isNC",
-                    checkbox: {
-                        equals: false
-                    }
-                }
-            ]
+      database_id: dbId,
+      filter: {
+        and: [
+          {
+            property: "isAdmin",
+            checkbox: {
+              equals: false,
+            },
+          },
+          {
+            property: "isBanned",
+            checkbox: {
+              equals: false,
+            },
+          },
+          {
+            property: "isNC",
+            checkbox: {
+              equals: false,
+            },
+          },
+        ],
+      },
+      sorts: [
+        {
+          property: "points",
+          direction: "descending",
         },
-        sorts: [
-            {
-                property: 'points',
-                direction: 'descending'
-            }  
-        ]
-    })
-    const users = usersN.results.map(user => {
-        return {
-            name: user.properties.Name.rich_text[0].text.content,
-            points: user.properties.points.rich_text[0].text.content,
-            email: user.properties.email.title[0].plain_text,
-            displayName: user.properties.displayName.rich_text[0].text.content,
-            currentLevel: user.properties.currentLevel.rich_text[0].text.content,
-        }
-    })
-    console.log(userLog)
-    res.render('leaderboard', {users,userLog: req.user})
-} catch (err) {
-    console.log(err)
-    SendMessage(err.stack.toString())
-    SendMessage('The server has crashed')
-    res.redirect('/')
-}
-})
+      ],
+    });
+    const users = usersN.results.map((user) => {
+      return {
+        name: user.properties.Name.rich_text[0].text.content,
+        points: user.properties.points.rich_text[0].text.content,
+        email: user.properties.email.title[0].plain_text,
+        displayName: user.properties.displayName.rich_text[0].text.content,
+        currentLevel: user.properties.currentLevel.rich_text[0].text.content,
+      };
+    });
+    console.log(userLog);
+    res.render("leaderboard", { users, userLog: req.user });
+  } catch (err) {
+    console.log(err);
+    SendMessage(err.stack.toString());
 
-router.get('/banned', async (req,res) => { 
-    if (req.cookies.token) {
-        const token = req.cookies.token
-        const decoded = jwt.verify(token, jwt_token);
-            const user = await notion.databases.query({
-                database_id: process.env.NOTION_DB_ID,
-                filter: {
-                    and: [
-                        {
-                            property: 'email',
-                            title: {
-                                equals: decoded.email
-                            }
-                        }
-                    ]
-                }
-            })
-        if (user) {
-            req.user =  {
-                email: user.results[0].properties.email.title[0].plain_text, 
-                name: user.results[0].properties.Name.rich_text[0].text.content,
-                displayName: user.results[0].properties.displayName.rich_text[0].text.content
-            }
-        }
+    res.redirect("/");
+  }
+});
+
+router.get("/banned", async (req, res) => {
+  if (req.cookies.token) {
+    const token = req.cookies.token;
+    const decoded = jwt.verify(token, jwt_token);
+    const user = await notion.databases.query({
+      database_id: process.env.NOTION_DB_ID,
+      filter: {
+        and: [
+          {
+            property: "email",
+            title: {
+              equals: decoded.email,
+            },
+          },
+        ],
+      },
+    });
+    if (user) {
+      req.user = {
+        email: user.results[0].properties.email.title[0].plain_text,
+        name: user.results[0].properties.Name.rich_text[0].text.content,
+        displayName:
+          user.results[0].properties.displayName.rich_text[0].text.content,
+      };
     }
-    const userLog = req.user
-    res.render('banned', {userLog})
-})
+  }
+  const userLog = req.user;
+  res.render("banned", { userLog });
+});
 
-module.exports= router;
+module.exports = router;
